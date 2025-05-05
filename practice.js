15cd9c865db2530038b15c7363d1392509f1165f
kim177t <kimberly.tran@mail.citytech.cuny.edu>
2025-05-04 21:27:25 -0400

    testing git ignore

--- practice.js
+++ practice.js
@@ -4,6 +4,7 @@ let userInput = prompt('What is your name?');
 console.log(userInput);
 if (userInput === 'Kim') {
     console.log('Hi Kim!');
+    userInput = prompt('Is this the Kim from Queens?')
 } else {
     console.log('Who?');
 }
