d8cbaf22cccc5e14feab49d01581c410ff4ef523
mikfairo <mikfairo123@gmail.com>
2025-05-08 23:58:36 -0400

    Solved a hard mode problem for accessing items in an array and key values in a complex object.

--- testingobjects.js
+++ testingobjects.js
@@ -23,64 +23,114 @@
 
 // console.log(myObject2.users.kelly.gender);
 
-const mediumObject = {
-    level1: {
-      level2: {
+// const mediumObject = {
+//     level1: {
+//       level2: {
+//         level3: {
+//           level4: {
+//             level5: {
+//               level6: {
+//                 level7: {
+//                   level8: [
+//                     {
+//                       level9: {
+//                         level10: [
+//                           {
+//                             level11: {
+//                               level12: [
+//                                 [],
+//                                 {
+//                                   level13: [
+//                                     {
+//                                       level14: {
+//                                         level15: [
+//                                           [],
+//                                           [],
+//                                           {
+//                                             level16: {
+//                                               level17: [
+//                                                 {
+//                                                   level18: {
+//                                                     level19: {
+//                                                       level20: "You made it!"
+//                                                     }
+//                                                   }
+//                                                 }
+//                                               ]
+//                                             }
+//                                           }
+//                                         ]
+//                                       }
+//                                     }
+//                                   ]
+//                                 }
+//                               ]
+//                             }
+//                           }
+//                         ]
+//                       }
+//                     }
+//                   ]
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   };
+
+//   console.log(mediumObject. level1.level2.level3.level4.level5.level6.level7.level8[0].level9.level10[0].level11.level12[1].level13[0].level14.level15[2].level16.level17[0].level18.level19.level20);
+
+
+const hardObj = {
+  level1: {
+    level2: [
+      { level3: [] },
+      {
         level3: {
-          level4: {
-            level5: {
-              level6: {
-                level7: {
-                  level8: [
-                    {
-                      level9: {
-                        level10: [
+          level4: [
+            [],
+            {
+              level5: [
+                {
+                  level6: {
+                    level7: [
+                      [],
+                      [],
+                      {
+                        level8: [
+                          { level9: [] },
                           {
-                            level11: {
-                              level12: [
-                                [],
-                                {
-                                  level13: [
-                                    {
-                                      level14: {
-                                        level15: [
-                                          [],
-                                          [],
-                                          {
-                                            level16: {
-                                              level17: [
-                                                {
-                                                  level18: {
-                                                    level19: {
-                                                      level20: "You made it!"
-                                                    }
-                                                  }
-                                                }
-                                              ]
-                                            }
-                                          }
-                                        ]
-                                      }
+                            level9: [
+                              {},
+                              {
+                                level10: [
+                                  [],
+                                  {
+                                    level11: {
+                                      level12: [
+                                        { level13: [] },
+                                        { level13: { level14: [[], [], { level15: { level16: [{ level17: { level18: [{ level19: [{ level20: "You made it on hard object!" }] }] } }] } }] }  },
+                                        { level13: [] }]
+                                  }}
+                                      ]
                                     }
-                                  ]
-                                }
-                              ]
-                            }
+                         ] },
+                                  []
+                                ]
+                              },
+                              {}
+                            ]
                           }
+                          },
                         ]
                       }
-                    }
-                  ]
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  };
-
-  console.log(mediumObject. level1.level2.level3.level4.level5.level6.level7.level8[0].level9.level10[0].level11.level12[1].level13[0].level14.level15[2].level16.level17[0].level18.level19.level20);
-
-
-
+                    ]
+                  }
+                },
+                { level6: [] }
+              ]
+            },
+};
+console.log(hardObj.level1.level2[1].level3.level4[1].level5[0].level6.level7[2].level8[1].level9[1].level10[1].level11.level12[1].level13.level14[2].level15.level16[0].level17.level18[0].level19[0].level20);
