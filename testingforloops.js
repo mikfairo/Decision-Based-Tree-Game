e887d0c3aba743e39c5cc89fedfa08d8e5c53dbb
mikfairo <mikfairo123@gmail.com>
2025-05-13 23:52:49 -0400

    Practiced for loops, string literals and index notations.

---
+++ testingforloops.js
@@ -0,0 +1,40 @@
+// for (let i = 0; i < 10; i++) {
+//     // console.log(i)
+// }
+//console.log(1 < 1)
+// console.log(2=='2') 
+// console.log(typeof 2)
+// console.log(typeof '2')
+// for (let i = 0; i < 10; i = i+2){
+//    console.log(i) 
+// }
+// for (let i = 0; i <100; i = i+5){
+//     console.log(i)
+// }
+// for (let i = 0; i < 100; i++){
+//     if (i % 2 === 0){
+//         console.log(`${i} is even`)
+        
+//     }
+//     if (i % 2 === 1){
+//         console.log(`${i} is odd`)
+//     }
+//     if (i % 10 === 0){
+//         console.log(`${i} is divisible by 10`)
+//     }
+// }
+
+
+// let x = 10; 
+// let y = 5; 
+// if (x + y === 15){
+//     console.log('yes x + y = 15')
+// }
+// let x = 7;
+// console.log(`this number is ${x}`)
+
+let groceryList = ['banana','pineapple','strawberry','pear','grape','lychee']
+for (let i = 0; i < groceryList.length; i++) {
+    console.log(groceryList[i])
+}
+
